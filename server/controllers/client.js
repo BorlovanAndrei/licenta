8a73ee7bac6896b0f07cbd44ceaef552d171fd25
Borlovan Andrei <andrei.borlovan@icloud.com>
2024-03-18 15:46:21 +0200

    no planstat

--- server/controllers/client.js
+++ server/controllers/client.js
@@ -1,5 +1,5 @@
 import Plan from "../models/Plan.js"
-import PlanStat from "../models/PlanStat.js"
+//import PlanStat from "../models/PlanStat.js"
 import Transaction from "../models/Transaction.js";
 import User from "../models/User.js";
 
@@ -8,18 +8,18 @@ export const getPlans = async (req, res) =>{
     try{
         const plans = await Plan.find();
 
-        const plansWithStats = await Promise.all(
-            plans.map(async (plan) =>{
-                const stat = await PlanStat.find({
-                    planId: plan._id
-                });
-                return{
-                    ...plan._doc,
-                    stat,
-                };
-            })
-        );
-        res.status(200).json(plansWithStats);
+        // const plansWithStats = await Promise.all(
+        //     plans.map(async (plan) =>{
+        //         const stat = await PlanStat.find({
+        //             planId: plan._id
+        //         });
+        //         return{
+        //             ...plan._doc,
+        //             stat,
+        //         };
+        //     })
+        // );
+        res.status(200).json(plans);
     }catch{
         res.status(404).json({message: error.message});
     }
