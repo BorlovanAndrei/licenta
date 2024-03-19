36adea49d9a55dfdb86749f53f6d3ffb61666c68
Borlovan Andrei <andrei.borlovan@icloud.com>
2024-03-19 15:40:51 +0200

    upd finish operations

--- server/controllers/management.js
+++ server/controllers/management.js
@@ -72,44 +72,6 @@ export const deleteEquipment = async (req, res) =>{
 };
 
 
-//Operations
-// export const getOperations = async (req, res) => {
-//     try {
-//         const { page = 1, pageSize = 20, sort = null, search = "" } = req.query;
-    
-//         const generateSort = () => {
-//           const sortParsed = JSON.parse(sort);
-//           const sortFormatted = {
-//             [sortParsed.field]: (sortParsed.sort = "asc" ? 1 : -1),
-//           };
-    
-//           return sortFormatted;
-//         };
-//         const sortFormatted = Boolean(sort) ? generateSort() : {};
-
-//         const operation = await Operation.find({
-//           $or: [
-//             { cost: { $regex: new RegExp(search, "i") } },
-//             { equipmentId: { $regex: new RegExp(search, "i") } },
-//           ],
-//         })
-//           .sort(sortFormatted)
-//           .skip(page * pageSize)
-//           .limit(pageSize);
-    
-//         const total = await Operation.countDocuments({
-//           name: { $regex: search, $options: "i" },
-//         });
-    
-//         res.status(200).json({
-//             operation,
-//             total,
-//         });
-//       } catch (error) {
-//         res.status(404).json({ message: error.message });
-//       }
-// }
-
 
 export const getOperations = async (req, res) => {
     try {
